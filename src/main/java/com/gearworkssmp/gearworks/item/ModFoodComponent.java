89ae8b2f302ff359a47436c770d7d6c72112f7cb
uberswe <m@rkus.io>
2024-09-19 13:49:07 +0200

    tweak cog cupcake hunger

--- src/main/java/com/gearworkssmp/gearworks/item/ModFoodComponent.java
+++ src/main/java/com/gearworkssmp/gearworks/item/ModFoodComponent.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponent {
-	public static final FoodComponent COG_CUPCAKE = new FoodComponent.Builder().hunger(3).saturationModifier(0.25f)
+	public static final FoodComponent COG_CUPCAKE = new FoodComponent.Builder().hunger(5).saturationModifier(0.25f)
 			.statusEffect(new StatusEffectInstance(StatusEffects.LUCK, 2400), 1.0f).build();
 	public static final FoodComponent INCOMPLETE_COG_CUPCAKE = new FoodComponent.Builder()
 			.statusEffect(new StatusEffectInstance(StatusEffects.POISON, 600), 0.8f).build();
